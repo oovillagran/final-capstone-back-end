9a63cfad0f712b9672c8e39c3324309ab60b580f
eric <ericopokuantwi384@gmail.com>
2023-08-25 21:30:38 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "users#index"
+  namespace :api do
+    namespace :v1 do
+      post 'sessions' => 'sessions#create'
+      
+      resources :users, only: [] do
+        resources :appointments, only: [:index, :create, :update, :destroy]
+      end
+      
+      resources :doctors, only: [:index, :show]
+      resources :clinics, only: [:index, :show]
+      resources :specialties, only: [:index, :create, :destroy]
+      resources :reservations, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+  
+  devise_for :users, controllers: { passwords: 'devise/passwords' }
+  root "users#index"
+  
+  resources :users, only: [:index, :show] do
+    resources :appointments, only: [:index, :show, :new, :create, :destroy]
+  end
+  
+  resources :doctors, only: [:show]
 end
