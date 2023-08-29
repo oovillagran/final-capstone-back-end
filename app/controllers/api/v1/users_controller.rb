0cb08bdc0bdfecdd95c479e53481d8157110d6a7
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-08-29 12:40:44 -0300

    added swagger directory

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,4 +12,25 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
     render json: @user
   end
+
+  # GET /api/v1/users/{id}
+  swagger_path '/api/v1/users/{id}' do
+    operation :get do
+      key :summary, 'Fetch a user by ID'
+      key :tags, ['users']
+      parameter do
+        key :name, :id
+        key :in, :path
+        key :description, 'User ID'
+        key :required, true
+        key :type, :integer
+      end
+      response 200 do
+        key :description, 'User response'
+        schema do
+          key :$ref, :User
+        end
+      end
+    end
+  end
 end
---
+++ config/initializers/swagger_docs.rb
@@ -0,0 +1,3 @@
+Rswag::Api.configure do |config|
+  config.swagger_root = Rails.root.to_s + '/swagger'
+end
