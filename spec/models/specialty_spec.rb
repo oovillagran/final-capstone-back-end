a9138bdc6d31ffaa6539e4a014b0578a4b3ed244
Eric Antwi <ericopokuantwi384@gmail.com>
2023-08-30 14:38:02 +0000

    Update specialty_spec.rb

--- spec/models/specialty_spec.rb
+++ spec/models/specialty_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Specialty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(
+      first_name: 'Eric',
+      last_name: 'Antwi',
+      birthdate: Date.parse('30-09-2001'),
+      role: 'user',
+      phone_number: '+233206546917',
+      username: 'Mylo16',
+      email: 'ericopokuantwi384@gmail.com',
+      password: '123456'
+    )
+    @doctor = Doctor.create(name: 'Eric Antwi', photo: 'myprofile.png', bio: 'I am a dentist', experience_years: '3',
+                            user_id: @user.id)
+    @specialty = Specialty.create(specialty_type: 'Dentistry', doctor_id: @doctor.id)
+  end
+
+  describe 'Specialty validations' do
+    it 'has valid attributes' do
+      expect(@specialty).to be_valid
+    end
+  end
 end
