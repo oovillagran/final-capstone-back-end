66322b945872bd23d6e56ac2b0f2b0e9d191c09d
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-08-30 11:22:57 -0300

    Revert "Merge branch 'test' into dev"
    This reverts commit c1b7bc06507481a9432199dd122b78910eff6677, reversing
    changes made to d8cc64a73bd9a104a29b00a2750ef22ec901716b.

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,39 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  before(:each) do
-    @user = User.create(
-      first_name: 'Eric',
-      last_name: 'Antwi',
-      birthdate: Date.parse('30-09-2001'),
-      role: 'user',
-      phone_number: '+233206546917',
-      username: 'Mylo16',
-      email: 'ericopokuantwi384@gmail.com',
-      password: '123456'
-    )
-    @doctor = Doctor.create(name: 'Eric Antwi', photo: 'myprofile.png', bio: 'I am a dentist', experience_years: '3',
-                            user_id: @user.id)
-  end
-
-  describe 'Doctor validations' do
-    it 'has valid attributes' do
-      expect(@doctor).to be_valid
-    end
-
-    it 'is not valid without a bio' do
-      @doctor.bio = nil
-      expect(@doctor).to_not be_valid
-    end
-
-    it 'is not valid without a name' do
-      @doctor.name = nil
-      expect(@doctor).to_not be_valid
-    end
-
-    # it 'is not valid without a photo' do
-    #   @doctor.photo = nil
-    #   expect(@doctor).to_not be_valid
-    # end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
