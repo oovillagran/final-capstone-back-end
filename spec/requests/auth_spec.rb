289f8fa0ff426506a8cdf587d1f7e3d7b2d0872b
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-08-31 18:48:34 -0300

    added authentication spec

---
+++ spec/requests/auth_spec.rb
@@ -0,0 +1,55 @@
+require 'swagger_helper'
+
+RSpec.describe 'auth', type: :request do
+
+  path '/login' do
+
+    post('login auth') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/auto_login' do
+
+    get('auto_login auth') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/user_is_authed' do
+
+    get('user_is_authed auth') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
